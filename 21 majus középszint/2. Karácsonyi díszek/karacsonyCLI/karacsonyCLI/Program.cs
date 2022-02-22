d7078666ec22233eff16424e8ad8d5bebd02b898
Megyesi Richard <43165781+MegyesiRicsi@users.noreply.github.com>
2022-02-22 18:32:04 +0100

    Add files via upload

---
+++ 21 majus középszint/2. Karácsonyi díszek/karacsonyCLI/karacsonyCLI/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace karacsonyCLI
+{
+    class NapiMunka
+    {
+        public static int KeszultDb { get; private set; }
+        public int Nap { get; private set; }
+        public int HarangKesz { get; private set; }
+        public int HarangEladott { get; private set; }
+        public int AngyalkaKesz { get; private set; }
+        public int AngyalkaEladott { get; private set; }
+        public int FenyofaKesz { get; private set; }
+        public int FenyofaEladott { get; private set; }
+
+        public NapiMunka(string sor)
+        {
+            string[] s = sor.Split(';');
+            Nap = Convert.ToInt32(s[0]);
+            HarangKesz = Convert.ToInt32(s[1]);
+            HarangEladott = Convert.ToInt32(s[2]);
+            AngyalkaKesz = Convert.ToInt32(s[3]);
+            AngyalkaEladott = Convert.ToInt32(s[4]);
+            FenyofaKesz = Convert.ToInt32(s[5]);
+            FenyofaEladott = Convert.ToInt32(s[6]);
+
+            NapiMunka.KeszultDb += HarangKesz + AngyalkaKesz + FenyofaKesz;
+        }
+
+        public int NapiBevetel()
+        {
+            return -(HarangEladott * 1000 + AngyalkaEladott * 1350 + FenyofaEladott * 1500);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<NapiMunka>();
+            var sr = new StreamReader("diszek.txt");
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new NapiMunka(sr.ReadLine()));
+            }
+            var ossz = (from sor in lista 
+                        select sor.AngyalkaKesz+sor.FenyofaKesz+sor.HarangKesz);
+            Console.WriteLine($"4.feladat: Összesen {ossz.Sum()} darab dísz készült.");
+            bool volte = false;
+            foreach (var item in ossz)
+            {
+                if (item==0)
+                {
+                    volte = true;
+                    break;
+                }
+            }
+            if (volte)
+            {
+                Console.WriteLine("5. feladat: Volt olyan nap, amikor egyetlen dísz sem készült");
+            }
+            else
+            {
+                Console.WriteLine("5. feladat: Nem volt olyan nap, amikor egyetlen dísz sem készült");
+            }
+
+            int nap = 0;
+            do
+            {
+                Console.Write("\nAdja meg a keresett napot [1 ... 40]: ");
+                nap = int.Parse(Console.ReadLine());
+            } while (nap > 40 || nap<1);
+            var megadott = (from sor in lista where sor.Nap == nap select sor);
+            int maradtA = 0;
+            int maradtF = 0;
+            int maradtH = 0;
+            foreach (var item in lista)
+            {
+                if (item.Nap<16)
+                {
+                    maradtA = maradtA + item.AngyalkaKesz + item.AngyalkaEladott;
+                    maradtF = maradtF + item.FenyofaKesz + item.FenyofaEladott;
+                    maradtH = maradtH + item.HarangKesz + item.HarangEladott;
+                }
+               
+               
+            }
+            Console.WriteLine($"A(z) {nap} végé {maradtH} harang, {maradtA} angyalka és {maradtF} fenyőfa maradt készleten.");
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 21 majus középszint/3. Trolibusz/adatbazis/megoldasok.sql
@@ -0,0 +1,35 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+-- 10. feladat:
+CREATE DATABASE halozat DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+
+-- 12. feladat:
+
+INSERT into megallok (id, nev) VALUES (198, "Kőbányai garázs");
+-- 13. feladat:
+UPDATE jaratok SET Elsoajtos=0 WHERE id=20
+
+-- 14. feladat:
+SELECT jaratSzam from jaratok WHERE elsoAjtos=1
+
+-- 15. feladat:
+SELECT nev from megallok where nev like "%sétány" ORDER by nev
+
+-- 16. feladat:
+SELECT halozat.sorszam sorszam, megallok.nev megallo 
+from megallok 
+INNER JOIN halozat on megallok.id=halozat.megallo 
+INNER JOIN jaratok on halozat.jarat=jaratok.id 
+WHERE jaratok.jaratSzam="CITY" AND halozat.irany="A" 
+ORDER by halozat.sorszam
+
+-- 17. feladat:
+SELECT megallok.nev megallo, COUNT(halozat.jarat) jaratokSzama
+ from megallok
+ INNER JOIN halozat on megallok.id=halozat.megallo
+ GROUP by halozat.megallo 
+ HAVING COUNT(halozat.jarat)>3
+ ORDER by megallok.nev
+
+
